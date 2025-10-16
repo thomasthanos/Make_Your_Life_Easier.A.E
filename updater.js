33b16f4424eac5c69e3a63f0e182b49e30023ad6
thomasthanos <thomasthanos28@gmail.com>
2025-10-16 03:29:34 +0300

    patch

--- updater.js
+++ updater.js
@@ -11,55 +11,64 @@ class AppUpdater {
     autoUpdater.autoDownload = false;
     autoUpdater.autoInstallOnAppQuit = true;
 
-autoUpdater.on('checking-for-update', () => {
-  this.sendStatusToWindow('checking');
-});
+    autoUpdater.on('checking-for-update', () => {
+      this.sendStatusToWindow('Checking for update...');
+    });
 
-autoUpdater.on('update-available', (info) => {
-  this.sendStatusToWindow('available', {
-    version: info.version,
-    releaseDate: info.releaseDate,
-    releaseNotes: info.releaseNotes
-  });
-});
+    autoUpdater.on('update-available', (info) => {
+      this.sendStatusToWindow('Update available.');
+      
+      // Ask user if they want to download the update
+      dialog.showMessageBox(this.mainWindow, {
+        type: 'info',
+        title: 'Update Available',
+        message: `A new version ${info.version} is available. Do you want to download it now?`,
+        buttons: ['Download', 'Later']
+      }).then((result) => {
+        if (result.response === 0) {
+          autoUpdater.downloadUpdate();
+        }
+      });
+    });
 
-autoUpdater.on('update-not-available', (info) => {
-  this.sendStatusToWindow('not-available', {
-    version: info?.version
-  });
-});
-autoUpdater.on('error', (err) => {
-  this.sendStatusToWindow('error', {
-    message: err.message
-  });
-});
+    autoUpdater.on('update-not-available', (info) => {
+      this.sendStatusToWindow('Update not available.');
+    });
 
-autoUpdater.on('download-progress', (progressObj) => {
-  this.sendStatusToWindow('downloading', {
-    percent: progressObj.percent,
-    bytesPerSecond: progressObj.bytesPerSecond,
-    transferred: progressObj.transferred,
-    total: progressObj.total
-  });
-});
+    autoUpdater.on('error', (err) => {
+      this.sendStatusToWindow('Error in auto-updater: ' + err);
+    });
 
-autoUpdater.on('update-downloaded', (info) => {
-  this.sendStatusToWindow('downloaded', {
-    version: info.version,
-    releaseDate: info.releaseDate
-  });
-});
-  }
+    autoUpdater.on('download-progress', (progressObj) => {
+      let log_message = "Download speed: " + progressObj.bytesPerSecond;
+      log_message = log_message + ' - Downloaded ' + Math.round(progressObj.percent) + '%';
+      log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+      this.sendStatusToWindow(log_message);
+    });
 
-sendStatusToWindow(status, data = {}) {
-  console.log('Updater:', status, data);
-  if (this.mainWindow && this.mainWindow.webContents) {
-    this.mainWindow.webContents.send('update-status', {
-      status: status,
-      ...data
+    autoUpdater.on('update-downloaded', (info) => {
+      this.sendStatusToWindow('Update downloaded; will install in 5 seconds');
+      
+      // Ask user to restart and install update
+      dialog.showMessageBox(this.mainWindow, {
+        type: 'info',
+        title: 'Update Ready',
+        message: 'Update downloaded. Restart the application to apply the update?',
+        buttons: ['Restart', 'Later']
+      }).then((result) => {
+        if (result.response === 0) {
+          autoUpdater.quitAndInstall();
+        }
+      });
     });
   }
-}
+
+  sendStatusToWindow(text) {
+    console.log('Updater:', text);
+    if (this.mainWindow && this.mainWindow.webContents) {
+      this.mainWindow.webContents.send('updater-message', text);
+    }
+  }
 
   checkForUpdates() {
     autoUpdater.checkForUpdates();
