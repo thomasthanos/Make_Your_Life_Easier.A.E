3f27f911b12f7bed9e610d186e986677df035109
Thomas <116557800+thomasthanos@users.noreply.github.com>
2026-08-23 14:40:13 +0300

    patch

---
+++ tools/update-version.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const packageJson = require('../package.json');
+const version = packageJson.version;
+
+const bannerPath = path.join(__dirname, '../.github/assets/banner-myle.svg');
+let svgContent = fs.readFileSync(bannerPath, 'utf8');
+
+// Replace the version string vX.X.X with the new version
+svgContent = svgContent.replace(/>v\d+\.\d+\.\d+</, '>v' + version + '<');
+
+fs.writeFileSync(bannerPath, svgContent);
+console.log('Updated banner SVG to version v' + version);
